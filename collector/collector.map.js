c6acaeca0d3cc4f68b93fe8e1a02bd2f6ef00562
khanal.nishant <khanal.nishant@gmail.com>
2018-01-14 01:53:13 -0500

    created readme.md for collector

---
+++ collector/collector.map.js
@@ -0,0 +1,22 @@
+var collector = {}
+
+//initialize constants for collector application
+collector.createConstants = function(){
+  
+};
+
+//create helpers and functions
+collector.createFunctions = function(){
+  
+};
+
+collector.initializeUI = function(){
+  
+};
+
+collector.init = function(){
+  collector.createConstants();
+  collector.createFunctions();
+  collector.initializeUI();
+  
+}
